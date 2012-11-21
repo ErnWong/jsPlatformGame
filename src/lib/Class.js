5b7763e1b26f793a2dd7962f3b1e84946f3df8d8
ErnWong <ewon@gmail.com>
2012-11-21 20:15:47 +1300

    Tidied up Class.js

--- src/lib/Class.js
+++ src/lib/Class.js
@@ -7,13 +7,27 @@
 
 "use strict";
 
-if ( typeof lib == null ) lib = {};
+if ( window.lib == null ) {
+  window.lib = {};
+}
 
-var createToString = function( value ) {
-  return function toString() {
-    return value;
+var initialising = false,
+  testFor_super = ( /abc/.test( function(){abc;} ) )? /\b_parent\b/ : /.*/,
+  ConstructorCallException = new TypeError("Class constructor cannot be called as a function."),
+  createToString = function( value ) {
+    return function toString() {
+      return value;
+    };
+  },
+  modFn = function( name, fn, _super ) { //do this or anonymous function inside extend()? TODO: test for speed
+    return function() {
+      var tmp = this._super;
+      this._super = _super[name];
+      var ret = fn.apply( this, arguments );
+      this._super = tmp;
+      return ret;
+    };
   };
-};
 
 lib.Class = function Class() {
 
@@ -22,29 +36,14 @@ lib.Class = function Class() {
 lib.Class.prototype.toString = createToString( "[object Class]" );
 lib.Class.toString = createToString( "function Class() { [lib code] }" );
 
-lib.Class.initializing = false;
-
-var ConstructorCallException = new TypeError("Class constructor cannot be called as a function.");
-
-var testFor_super = ( /abc/.test( function(){abc;} ) )? /\b_parent\b/ : /.*/;
-var modFn = function( name, fn, super ) { //do this or anonymous function inside extend()? TODO: test for speed
-  return function() {
-    var tmp = this._super;
-    this._super = super[name];
-    var ret = fn.apply( this, arguments );
-    this._super = tmp;
-    return ret;
-  };
-};
-
 if ( !window._DEBUG ) {
   lib.Class.extend = function extend( def, name, leaveToString ) {
 
     var _super = this.prototype;
 
-    this.initialising = true;
+    initialising = true;
     var prototype = new this();
-    this.initialising = false;
+    initialising = false;
 
     if ( name && !leaveToString ) {
       prototype.toString = createToString( "[object " + name + "]" );
@@ -52,16 +51,20 @@ if ( !window._DEBUG ) {
 
     for ( var id in def ) {
 
-      prototype[id] = (typeof def[id] === "function") &&
-        (typeof _parent[id] === "function") && testFor_super.test( def[id] ) ) ?
+      prototype[id] = typeof def[id] === "function" &&
+        typeof _super[id] === "function" && testFor_super.test( def[id] ) ?
            modFn( id, def[id], _super ) :
            def[id];
 
     }
 
     var Class = function Class() {
-      if ( !(this instanceof Class) ) throw ConstructorCallException;
-      if ( !this.initialising && this.init ) this.init.apply( this, arguments );
+      if ( !(this instanceof Class) ) {
+        throw ConstructorCallException;
+      }
+      if ( !initialising && this.init ) {
+        this.init.apply( this, arguments );
+      }
     };
     Class.prototype = prototype;
     Class.prototype.constructor = Class;
@@ -86,21 +89,25 @@ else {
 
     for ( var id in def ) {
 
-      prototype[id] = (typeof def[id] === "function") &&
-        (typeof _parent[id] === "function") && testFor_super.test( def[id] ) ) ?
+      prototype[id] = typeof def[id] === "function" &&
+        typeof _super[id] === "function" && testFor_super.test( def[id] ) ?
            modFn( id, def[id], _super ) :
            def[id];
 
     }
 
     var Class = name?
-      Function(   "return function " + name + "() {\n"
-                + "  if ( !(this isntanceof Class) ) throw new TypeError(\"Class constructor cannot be called as a function.\");\n"
-                + "  if ( !this.initialising && this.init ) this.init.apply( this, arguments );\n"
-                + "};" )() :
+      new Function( "return function " + name + "() {\n" +
+                    "  if ( !(this intanceof Class) ) {\n    throw new TypeError(\"Class constructor cannot be called as a function.\");\n  }\n" +
+                    "  if ( !this.initialising && this.init ) {\n    this.init.apply( this, arguments );\n  }\n" +
+                    "};" )() :
       function Class() {
-        if ( !(this instanceof Class) ) throw ConstructorCallException;
-        if ( !this.initialising && this.init ) this.init.apply( this, arguments );
+        if ( !(this instanceof Class) ) {
+          throw ConstructorCallException;
+        }
+        if ( !this.initialising && this.init ) {
+          this.init.apply( this, arguments );
+        }
       };
 
     Class.prototype = prototype;
@@ -112,8 +119,10 @@ else {
   };
 }
 
-lib.Class.extend.toString = creatToString( "function extend() { [lib code] }" );
+lib.Class.extend.toString = createToString( "function extend() { [lib code] }" );
 
-lib.loaded( "lib.Class" );
+if (lib.loaded) {
+  lib.loaded( "lib.Class" );
+}
 
 })(window);

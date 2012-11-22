00a71331675068d9f86053e7daa53450d97d2497
ErnWong <ewon@gmail.com>
2012-11-22 22:10:30 +1300

    Continued on the "onload" in lib.js

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -17,7 +17,8 @@ window._DEBUG = true;
   // a reference to an empty function
   EMPTY_FUNCTION = Function.prototype,
 
-  libInit,
+  libInit, libInitCallbacked = false,
+  libInitCallbacks = [];
 
 
 // if the head element is still not defined
@@ -27,6 +28,11 @@ if ( !head ) {
   throw new ReferenceError( "Where's the head? Cannot find the \"head\" element in the document." );
 
 }
+
+lib.onload = function ( fn ) {
+  libInitCallbacks.push( fn );
+  if ( libInitCallbacked ) fn.call( window );
+};
 
 
 // if JSON is not defined and we haven't already tried adding one,
@@ -70,191 +76,195 @@ if ( !window.JSON/*&& !triedJSON*/ ) {
 
 libInit = function libInit() {
 
-// a Array.prototype.indexOf "polyfill"
-// - based on mozilla's, which uses ECMA-262, 5th edition's algorithm
-if ( ![].indexOf ) {
-  Array.prototype.indexOf = function( searchEl ) {
-    if ( this == null ) {
-      throw new TypeError("Array.prototype.indexOf called on null or undefined");
-    }
-    var t = Object(this);
-    var len = t.length >>> 0;
-    if ( len === 0 ) {
-      return -1;
-    }
-    var n = 0;
-    if ( arguments.length > 1 ) {
-      n = Number( arguments[1] );
-      if (n != n) {
-        n = 0;
-      } else if ( n != 0 && n != Infinity && n != -Infinity ) {
-        n = (n > 0 || -1) * Math.floor( Math.abs(n) );
+  // a Array.prototype.indexOf "polyfill"
+  // - based on mozilla's, which uses ECMA-262, 5th edition's algorithm
+  if ( ![].indexOf ) {
+    Array.prototype.indexOf = function( searchEl ) {
+      if ( this == null ) {
+        throw new TypeError("Array.prototype.indexOf called on null or undefined");
       }
-    }
-    if ( n >= len ) {
-      return -1;
-    }
-    var k = n >= 0 ? n : Math.max( len - Math.abs(n), 0 );
-      for ( ; k < len; k++ ) {
-        if ( k in t && t[k] === searchEl ) {
-          return k;
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if ( len === 0 ) {
+        return -1;
+      }
+      var n = 0;
+      if ( arguments.length > 1 ) {
+        n = Number( arguments[1] );
+        if (n != n) {
+          n = 0;
+        } else if ( n != 0 && n != Infinity && n != -Infinity ) {
+          n = (n > 0 || -1) * Math.floor( Math.abs(n) );
         }
       }
-    return -1;
-  };
-}
+      if ( n >= len ) {
+        return -1;
+      }
+      var k = n >= 0 ? n : Math.max( len - Math.abs(n), 0 );
+        for ( ; k < len; k++ ) {
+          if ( k in t && t[k] === searchEl ) {
+            return k;
+          }
+        }
+      return -1;
+    };
+  }
 
-var requested, executed, scriptToRequest, scriptIdToURL, ScriptRequest, loadedStrict,
+  var requested, executed, scriptToRequest, scriptIdToURL, ScriptRequest, loadedStrict,
 
-  createOnReadyStateChangeListener = function( scriptURL ) {
-    return function() {
-      if ( this.readyState === "loaded" || this.readyState === "complete" ) {
+    createOnReadyStateChangeListener = function( scriptURL ) {
+      return function() {
+        if ( this.readyState === "loaded" || this.readyState === "complete" ) {
+          loadedStrict( scriptURL );
+        }
+      };
+    },
+
+    createOnLoadListener = function( scriptURL ) {
+      return function() {
         loadedStrict( scriptURL );
-      }
+      };
     };
-  },
 
-  createOnLoadListener = function( scriptURL ) {
-    return function() {
-      loadedStrict( scriptURL );
-    };
+
+  lib.requested = [];
+  requested = lib.requested;
+
+  lib.executed = [];
+  executed = lib.executed;
+
+  lib._scriptToRequest = {};
+  scriptToRequest = lib._scriptToRequest;
+
+  lib.scriptIdToURL = (function(path) {
+
+    try {
+
+      var request = window.XMLHttpRequest? new XMLHttpRequest() : new ActiveXObject( "Microsoft.XMlHTTP" );
+      request.open( "GET", path, false );
+      request.send();
+      if ( request.status >= 200 && request.status < 400 &&  !(request.responseText === null || request.responseText === "") ) {
+        if ( !JSON ) {
+          return ( new Function( "return " + request.responseText ) )();
+        }
+        return JSON.parse( request.responseText );
+      }
+
+    }
+    catch( err ) {
+    }
+    return {};
+
+  })( SCRIPT_ID_TO_URL_PATH );
+
+  scriptIdToURL = lib.scriptIdToURL;
+
+  lib._ScriptRequest = function( scriptCount, onload, loaded ) {
+    this.scriptCount = scriptCount || 0;
+    this.onload = onload || EMPTY_FUNCTION;
+    this.loaded = loaded || 0;
   };
 
+  lib._ScriptRequest.prototype = {
+    scriptCount: 0,
+    onload: EMPTY_FUNCTION,
+    loaded: 0
+  };
 
-lib.requested = [];
-requested = lib.requested;
+  ScriptRequest = lib._ScriptRequest;
 
-lib.executed = [];
-executed = lib.executed;
+  lib.require = function() {
 
-lib._scriptToRequest = {};
-scriptToRequest = lib._scriptToRequest;
-
-lib.scriptIdToURL = (function(path) {
-
-  try {
-
-    var request = window.XMLHttpRequest? new XMLHttpRequest() : new ActiveXObject( "Microsoft.XMlHTTP" );
-    request.open( "GET", path, false );
-    request.send();
-    if ( request.status >= 200 && request.status < 400 &&  !(request.responseText === null || request.responseText === "") ) {
-      if ( !JSON ) {
-        return ( new Function( "return " + request.responseText ) )();
-      }
-      return JSON.parse( request.responseText );
-    }
-
-  }
-  catch( err ) {
-  }
-  return {};
-
-})( SCRIPT_ID_TO_URL_PATH );
-
-scriptIdToURL = lib.scriptIdToURL;
-
-lib._ScriptRequest = function( scriptCount, onload, loaded ) {
-  this.scriptCount = scriptCount || 0;
-  this.onload = onload || EMPTY_FUNCTION;
-  this.loaded = loaded || 0;
-};
-
-lib._ScriptRequest.prototype = {
-  scriptCount: 0,
-  onload: EMPTY_FUNCTION,
-  loaded: 0
-};
-
-ScriptRequest = lib._ScriptRequest;
-
-lib.require = function() {
-
-  var argumentCount = arguments.length,
-
-    currentRequest = new ScriptRequest(argumentCount),
-
-    returnObject = {
-      onload: function( method ) {
-        currentRequest.onload = method;
-        if ( currentRequest.loaded === argumentCount ) {
-          method.call( window );
+    var i = 0,
+      argumentCount = arguments.length,
+      currentRequest = new ScriptRequest(argumentCount),
+      returnObject = {
+        onload: function( method ) {
+          currentRequest.onload = method;
+          if ( currentRequest.loaded === argumentCount ) {
+            method.call( window );
+          }
         }
+      };
+
+    for ( ; i < argumentCount; i++ ) {
+
+      var currentArg = scriptIdToURL[arguments[i]] || arguments[i];
+      if ( typeof currentArg !== "string" ) {
+        throw new TypeError( "Arguments needed to be a string, but found a " + typeof currentArg );
       }
-    };
 
-  for ( var i = 0; i < argumentCount; i++ ) {
-
-    var currentArg = scriptIdToURL[arguments[i]] || arguments[i];
-    if ( typeof currentArg !== "string" ) {
-      throw new TypeError( "Arguments needed to be a string, but found a " + typeof currentArg );
-    }
-
-    if ( requested.indexOf( currentArg ) !== -1 ) {
-      if ( executed.indexOf( currentArg ) !== -1 ) {
-        currentRequest.loaded++;
-      } else {
-        if ( scriptToRequest[currentArg] === undefined ) {
-          scriptToRequest[currentArg] = [];
+      if ( requested.indexOf( currentArg ) !== -1 ) {
+        if ( executed.indexOf( currentArg ) !== -1 ) {
+          currentRequest.loaded++;
+        } else {
+          if ( scriptToRequest[currentArg] === undefined ) {
+            scriptToRequest[currentArg] = [];
+          }
+          scriptToRequest[currentArg].push( currentRequest );
         }
-        scriptToRequest[currentArg].push( currentRequest );
+        continue;
       }
-      continue;
-    }
 
-    var newScript = document.createElement( "script" );
-    newScript.type = "text/javascript";
-    newScript.src = currentArg;
-    newScript.onreadystatechange = createOnReadyStateChangeListener( currentArg );
-    newScript.onload = createOnLoadListener( currentArg );
-    head.appendChild( newScript );
+      var newScript = document.createElement( "script" );
+      newScript.type = "text/javascript";
+      newScript.src = currentArg;
+      newScript.onreadystatechange = createOnReadyStateChangeListener( currentArg );
+      newScript.onload = createOnLoadListener( currentArg );
+      head.appendChild( newScript );
 
-    if ( scriptToRequest[currentArg] === undefined ) {
-      scriptToRequest[currentArg] = [];
-    }
-    scriptToRequest[currentArg].push( currentRequest );
-    requested.push( currentArg );
-
-  }
-
-  return returnObject;
-
-};
-
-lib.loaded = function( scriptId ) {
-  var scriptURL = scriptIdToURL[scriptId] || scriptId;
-  if ( executed.indexOf( scriptURL ) !== -1 ) {
-    return;
-  }
-  executed.push( scriptURL );
-  var scriptRequests = scriptToRequest[scriptURL];
-  if ( scriptRequests ) {
-    while ( scriptRequests.length /*> 0*/ ) {
-      scriptRequests[0].loaded++;
-      if ( scriptRequests[0].loaded === scriptRequests[0].scriptCount ) {
-        scriptRequests[0].onload.call( window );
+      if ( scriptToRequest[currentArg] === undefined ) {
+        scriptToRequest[currentArg] = [];
       }
-      scriptRequests.splice( 0, 1 );
-    }
-  }
-};
-loadedStrict = function( scriptURL ) {
-  if ( executed.indexOf( scriptURL ) !== -1 ) {
-    return;
-  }
-  executed.push( scriptURL );
-  var scriptRequests = scriptToRequest[scriptURL];
-  if ( scriptRequests ) {
-    while ( scriptRequests.length /*> 0*/ ) {
-      scriptRequests[0].loaded++;
-      if ( scriptRequests[0].loaded === scriptRequests[0].scriptCount ) {
-        scriptRequests[0].onload.call( window );
-      }
-      scriptRequests.splice( 0, 1 );
-    }
-  }
-};
+      scriptToRequest[currentArg].push( currentRequest );
+      requested.push( currentArg );
 
-window.lib = lib;
+    }
+
+    return returnObject;
+
+  };
+
+  lib.loaded = function( scriptId ) {
+    var scriptURL = scriptIdToURL[scriptId] || scriptId;
+    if ( executed.indexOf( scriptURL ) !== -1 ) {
+      return;
+    }
+    executed.push( scriptURL );
+    var scriptRequests = scriptToRequest[scriptURL];
+    if ( scriptRequests ) {
+      while ( scriptRequests.length /*> 0*/ ) {
+        scriptRequests[0].loaded++;
+        if ( scriptRequests[0].loaded === scriptRequests[0].scriptCount ) {
+          scriptRequests[0].onload.call( window );
+        }
+        scriptRequests.splice( 0, 1 );
+      }
+    }
+  };
+  loadedStrict = function( scriptURL ) {
+    if ( executed.indexOf( scriptURL ) !== -1 ) {
+      return;
+    }
+    executed.push( scriptURL );
+    var scriptRequests = scriptToRequest[scriptURL];
+    if ( scriptRequests ) {
+      while ( scriptRequests.length /*> 0*/ ) {
+        scriptRequests[0].loaded++;
+        if ( scriptRequests[0].loaded === scriptRequests[0].scriptCount ) {
+          scriptRequests[0].onload.call( window );
+        }
+        scriptRequests.splice( 0, 1 );
+      }
+    }
+  };
+
+  window.lib = lib;
+
+  for ( ; i < libInitCallbacksLength; i++ ) {
+    libInitCallbacks[i].call(window);
+    libInitCallbacked = true;
+  }
 
 }
 

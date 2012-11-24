f4e00e9214eb20f5e9473b46146220632b14d78f
ErnWong <ewon521@gmail.com>
2012-11-24 22:14:16 +1300

    Indented files

--- src/lib/Events.js
+++ src/lib/Events.js
@@ -1,88 +1,90 @@
 lib.requires( "lib.Class" ).onload( function() {
-"use strict";
+  "use strict";
 
-lib.Events = {};
-var _ = lib.Events,
-  ObjToString = Object.prototype.toString,
-  docCreateEvent = document.createEvent,
-  EvtTypeException = new TypeError( "Event object missing \"type\" property" ),
-  EvtListenerException = new TypeError( "Event Listener is not in the correct type" );
+  lib.Events = {};
+  var _ = lib.Events,
+    ObjToString = Object.prototype.toString,
+    docCreateEvent = document.createEvent,
+    EvtTypeException = new TypeError( "Event object missing \"type\" property" ),
+    EvtListenerException = new TypeError( "Event Listener is not in the correct type" );
 
-_.EventTarget = lib.Class.extend( {
+  _.EventTarget = lib.Class.extend( {
 
-  _listeners: {},
+    _listeners: {},
 
-  addEventListener: function addEventListener( type, listener, scope ) {
+    addEventListener: function addEventListener( type, listener, scope ) {
 
-    var thisListeners = this._listeners[type];
-    if ( !(thisListeners == null) && ( function() {
-        var thisArg = typeof scope === "object"? scope : null
-        for ( var i = 0, len = thisListeners.length; i < len; i++ ) {
-          if ( thisListeners[i].listener === listener && thisListeners[i].scope === thisArg ) {
-            return true;
+      var thisListeners = this._listeners[type];
+      if ( !(thisListeners == null) && ( function() {
+          var thisArg = typeof scope === "object"? scope : null
+          for ( var i = 0, len = thisListeners.length; i < len; i++ ) {
+            if ( thisListeners[i].listener === listener && thisListeners[i].scope === thisArg ) {
+              return true;
+            }
+          }
+          return false;
+        } )() ) {
+        return;
+      }
+      var newListener = {
+        listener: typeof listener === "object" && listener.handleEvent? listener.handleEvent : listener,
+        scope: typeof scope === "object"? scope : null
+      };
+      if ( ObjToString.call(thisListeners) === "[object Array]" ) {
+        thisListeners.push( newListener );
+      } else {
+        thisListeners = [newListener];
+      }
+
+    },
+
+    removeEventListener: function removeEventListener( type, listener, scope ) {
+      var thisListeners = this._listeners[type];
+      if ( ObjToString.call(thisListeners) === "[object Array]" ) {
+        var thisArg = typeof scope === "object"? scope : null,
+          i = 0,
+          len = thisListeners.length;
+        for ( ; i < len; i++ ) {
+          if ( thisListener[i].listener === listener && thisListener[i].scope = thisArg ) {
+            thisListener.splice( i, 1 );
+            return;
           }
         }
-        return false;
-      } )() ) {
-      return;
-    }
-    var newListener = {
-      listener: typeof listener === "object" && listener.handleEvent? listener.handleEvent : listener,
-      scope: typeof scope === "object"? scope : null
-    };
-    if ( ObjToString.call(thisListeners) === "[object Array]" ) {
-      thisListeners.push( newListener );
-    } else {
-      thisListeners = [newListener];
-    }
+      }
+    },
 
-  },
-
-  removeEventListener: function removeEventListener( type, listener, scope ) {
-    var thisListeners = this._listeners[type];
-    if ( ObjToString.call(thisListeners) === "[object Array]" ) {
-      var thisArg = typeof scope === "object"? scope : null,
-        i = 0,
-        len = thisListeners.length;
-      for ( ; i < len; i++ ) {
-        if ( thisListener[i].listener === listener && thisListener[i].scope = thisArg ) {
-          thisListener.splice( i, 1 );
-          return;
+    dispatchEvent: function dispatchEvent( evt ) {
+      /* stricter these days... or lazier
+        if ( typeof evt === "string" ) {
+          var type = evt;
+          evt = docCreateEvent( "event" );
+          evt.initEvent( type, false, false );
+        }
+      */
+      if ( !evt.target ) {
+        evt.target = this;
+      }
+      if ( !evt.type ) {
+        throw EvtTypeException;
+      }
+      var i = 0,
+        listeners = this._listeners[evt.type],
+        len = listeners;
+      if ( ObjToString.call( listeners ) === "[object Array]" ) {
+        for ( ; i < len; i++ ) {
+          var listener = listeners[i];
+          if ( typeof listener.listener !== "function ) throw EvtListenerException;
+          listener.listener.call( listener.scope, evt );
         }
       }
     }
-  },
 
-  dispatchEvent: function dispatchEvent( evt ) {
-    /* stricter these days... or lazier
-      if ( typeof evt === "string" ) {
-        var type = evt;
-        evt = docCreateEvent( "event" );
-        evt.initEvent( type, false, false );
-      }
-    */
-    if ( !evt.target ) {
-      evt.target = this;
-    }
-    if ( !evt.type ) {
-      throw EvtTypeException;
-    }
-    var i = 0,
-      listeners = this._listeners[evt.type],
-      len = listeners;
-    if ( ObjToString.call( listeners ) === "[object Array]" ) {
-      for ( ; i < len; i++ ) {
-        var listener = listeners[i];
-        if ( typeof listener.listener !== "function ) throw EvtListenerException;
-        listener.listener.call( listener.scope, evt );
-      }
-    }
-  }
+  }, "EventTarget" );
 
-}, "EventTarget" );
+  _.EventTarget.prototype.on = _.EventTarget.prototype.addEventListener;
 
-_.EventTarget.prototype.on = _.EventTarget.prototype.addEventListener;
+  _.somethingOutofTheOrdinaryIForgotWhatItIsCalled = blahblahblahblah_or_maybe_more_of_a_lib.Class.extend_or_something(STUFF);
 
-//TODO: _.EventTarget.prototype.on = ... see other Events.js
+  //TODO: _.EventTarget.prototype.on = ... see other Events.js
 
 } );
--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,274 +1,274 @@
 (function ( window, /*triedJSON,*/ undefined ) {
 
-"use strict";
+  "use strict";
 
-window._DEBUG = true;
+  window._DEBUG = true;
 
-  var lib = {},
+    var lib = {},
 
-  head = window.document.getElementsByTagName( "head" )[0],
+    head = window.document.getElementsByTagName( "head" )[0],
 
-  // the path (URL) to the script-id-to-path key (JSON)
-  SCRIPT_ID_TO_URL_PATH = "lib/Classes.js",
+    // the path (URL) to the script-id-to-path key (JSON)
+    SCRIPT_ID_TO_URL_PATH = "lib/Classes.js",
 
-  // the path (URL) to a JSON "polyfill"
-  JSON_PATH = "http://cdnjs.cloudflare.com/ajax/libs/json3/3.2.4/json3.min.js",
+    // the path (URL) to a JSON "polyfill"
+    JSON_PATH = "http://cdnjs.cloudflare.com/ajax/libs/json3/3.2.4/json3.min.js",
 
-  // a reference to an empty function
-  EMPTY_FUNCTION = Function.prototype,
+    // a reference to an empty function
+    EMPTY_FUNCTION = Function.prototype,
 
-  libInit, libInitCallbacked = false,
-  libInitCallbacks = [];
+    libInit, libInitCallbacked = false,
+    libInitCallbacks = [];
 
 
-// if the head element is still not defined
-if ( !head ) {
+  // if the head element is still not defined
+  if ( !head ) {
 
-  // throw a ReferenceError.
-  throw new ReferenceError( "Where's the head? Cannot find the \"head\" element in the document." );
+    // throw a ReferenceError.
+    throw new ReferenceError( "Where's the head? Cannot find the \"head\" element in the document." );
 
-}
-
-lib.onload = function ( fn ) {
-  libInitCallbacks.push( fn );
-  if ( libInitCallbacked ) {
-    fn.call( window );
   }
-};
 
-libInit = function libInit() {
+  lib.onload = function ( fn ) {
+    libInitCallbacks.push( fn );
+    if ( libInitCallbacked ) {
+      fn.call( window );
+    }
+  };
 
-  // a Array.prototype.indexOf "polyfill"
-  // - based on mozilla's, which uses ECMA-262, 5th edition's algorithm
-  if ( ![].indexOf ) {
-    Array.prototype.indexOf = function( searchEl ) {
-      if ( this == null ) {
-        throw new TypeError("Array.prototype.indexOf called on null or undefined");
-      }
-      var t = Object(this);
-      var len = t.length >>> 0;
-      if ( len === 0 ) {
-        return -1;
-      }
-      var n = 0;
-      if ( arguments.length > 1 ) {
-        n = Number( arguments[1] );
-        if (n != n) {
-          n = 0;
-        } else if ( n != 0 && n != Infinity && n != -Infinity ) {
-          n = (n > 0 || -1) * Math.floor( Math.abs(n) );
+  libInit = function libInit() {
+
+    // a Array.prototype.indexOf "polyfill"
+    // - based on mozilla's, which uses ECMA-262, 5th edition's algorithm
+    if ( ![].indexOf ) {
+      Array.prototype.indexOf = function( searchEl ) {
+        if ( this == null ) {
+          throw new TypeError("Array.prototype.indexOf called on null or undefined");
         }
-      }
-      if ( n >= len ) {
-        return -1;
-      }
-      var k = n >= 0 ? n : Math.max( len - Math.abs(n), 0 );
-        for ( ; k < len; k++ ) {
-          if ( k in t && t[k] === searchEl ) {
-            return k;
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if ( len === 0 ) {
+          return -1;
+        }
+        var n = 0;
+        if ( arguments.length > 1 ) {
+          n = Number( arguments[1] );
+          if (n != n) {
+            n = 0;
+          } else if ( n != 0 && n != Infinity && n != -Infinity ) {
+            n = (n > 0 || -1) * Math.floor( Math.abs(n) );
           }
         }
-      return -1;
-    };
-  }
+        if ( n >= len ) {
+          return -1;
+        }
+        var k = n >= 0 ? n : Math.max( len - Math.abs(n), 0 );
+          for ( ; k < len; k++ ) {
+            if ( k in t && t[k] === searchEl ) {
+              return k;
+            }
+          }
+        return -1;
+      };
+    }
 
-  var requested, executed, scriptToRequest, scriptIdToURL, ScriptRequest, loadedStrict,
+    var requested, executed, scriptToRequest, scriptIdToURL, ScriptRequest, loadedStrict,
 
-    createOnReadyStateChangeListener = function( scriptURL ) {
-      return function() {
-        if ( this.readyState === "loaded" || this.readyState === "complete" ) {
+      createOnReadyStateChangeListener = function( scriptURL ) {
+        return function() {
+          if ( this.readyState === "loaded" || this.readyState === "complete" ) {
+            loadedStrict( scriptURL );
+          }
+        };
+      },
+
+      createOnLoadListener = function( scriptURL ) {
+        return function() {
           loadedStrict( scriptURL );
-        }
+        };
       };
-    },
 
-    createOnLoadListener = function( scriptURL ) {
-      return function() {
-        loadedStrict( scriptURL );
-      };
+
+    lib.requested = [];
+    requested = lib.requested;
+
+    lib.executed = [];
+    executed = lib.executed;
+
+    lib._scriptToRequest = {};
+    scriptToRequest = lib._scriptToRequest;
+
+    lib.scriptIdToURL = (function(path) {
+
+      try {
+
+        var request = window.XMLHttpRequest? new XMLHttpRequest() : new ActiveXObject( "Microsoft.XMlHTTP" );
+        request.open( "GET", path, false );
+        request.send();
+        if ( request.status >= 200 && request.status < 400 &&  !(request.responseText === null || request.responseText === "") ) {
+          if ( !JSON ) {
+            return ( new Function( "return " + request.responseText ) )();
+          }
+          return JSON.parse( request.responseText );
+        }
+
+      }
+      catch( err ) {
+      }
+      return {};
+
+    })( SCRIPT_ID_TO_URL_PATH );
+
+    scriptIdToURL = lib.scriptIdToURL;
+
+    lib._ScriptRequest = function( scriptCount, onload, loaded ) {
+      this.scriptCount = scriptCount || 0;
+      this.onload = onload || EMPTY_FUNCTION;
+      this.loaded = loaded || 0;
     };
 
+    lib._ScriptRequest.prototype = {
+      scriptCount: 0,
+      onload: EMPTY_FUNCTION,
+      loaded: 0
+    };
 
-  lib.requested = [];
-  requested = lib.requested;
+    ScriptRequest = lib._ScriptRequest;
 
-  lib.executed = [];
-  executed = lib.executed;
+    lib.require = function() {
 
-  lib._scriptToRequest = {};
-  scriptToRequest = lib._scriptToRequest;
-
-  lib.scriptIdToURL = (function(path) {
-
-    try {
-
-      var request = window.XMLHttpRequest? new XMLHttpRequest() : new ActiveXObject( "Microsoft.XMlHTTP" );
-      request.open( "GET", path, false );
-      request.send();
-      if ( request.status >= 200 && request.status < 400 &&  !(request.responseText === null || request.responseText === "") ) {
-        if ( !JSON ) {
-          return ( new Function( "return " + request.responseText ) )();
-        }
-        return JSON.parse( request.responseText );
-      }
-
-    }
-    catch( err ) {
-    }
-    return {};
-
-  })( SCRIPT_ID_TO_URL_PATH );
-
-  scriptIdToURL = lib.scriptIdToURL;
-
-  lib._ScriptRequest = function( scriptCount, onload, loaded ) {
-    this.scriptCount = scriptCount || 0;
-    this.onload = onload || EMPTY_FUNCTION;
-    this.loaded = loaded || 0;
-  };
-
-  lib._ScriptRequest.prototype = {
-    scriptCount: 0,
-    onload: EMPTY_FUNCTION,
-    loaded: 0
-  };
-
-  ScriptRequest = lib._ScriptRequest;
-
-  lib.require = function() {
-
-    var i = 0,
-      argumentCount = arguments.length,
-      currentRequest = new ScriptRequest(argumentCount),
-      returnObject = {
-        onload: function( method ) {
-          currentRequest.onload = method;
-          if ( currentRequest.loaded === argumentCount ) {
-            method.call( window );
+      var i = 0,
+        argumentCount = arguments.length,
+        currentRequest = new ScriptRequest(argumentCount),
+        returnObject = {
+          onload: function( method ) {
+            currentRequest.onload = method;
+            if ( currentRequest.loaded === argumentCount ) {
+              method.call( window );
+            }
           }
+        };
+
+      for ( ; i < argumentCount; i++ ) {
+
+        var currentArg = scriptIdToURL[arguments[i]] || arguments[i];
+        if ( typeof currentArg !== "string" ) {
+          throw new TypeError( "Arguments needed to be a string, but found a " + typeof currentArg );
         }
-      };
 
-    for ( ; i < argumentCount; i++ ) {
-
-      var currentArg = scriptIdToURL[arguments[i]] || arguments[i];
-      if ( typeof currentArg !== "string" ) {
-        throw new TypeError( "Arguments needed to be a string, but found a " + typeof currentArg );
-      }
-
-      if ( requested.indexOf( currentArg ) !== -1 ) {
-        if ( executed.indexOf( currentArg ) !== -1 ) {
-          currentRequest.loaded++;
-        } else {
-          if ( scriptToRequest[currentArg] === undefined ) {
-            scriptToRequest[currentArg] = [];
+        if ( requested.indexOf( currentArg ) !== -1 ) {
+          if ( executed.indexOf( currentArg ) !== -1 ) {
+            currentRequest.loaded++;
+          } else {
+            if ( scriptToRequest[currentArg] === undefined ) {
+              scriptToRequest[currentArg] = [];
+            }
+            scriptToRequest[currentArg].push( currentRequest );
           }
-          scriptToRequest[currentArg].push( currentRequest );
+          continue;
         }
-        continue;
+
+        var newScript = document.createElement( "script" );
+        newScript.type = "text/javascript";
+        newScript.src = currentArg;
+        newScript.onreadystatechange = createOnReadyStateChangeListener( currentArg );
+        newScript.onload = createOnLoadListener( currentArg );
+        head.appendChild( newScript );
+
+        if ( scriptToRequest[currentArg] === undefined ) {
+          scriptToRequest[currentArg] = [];
+        }
+        scriptToRequest[currentArg].push( currentRequest );
+        requested.push( currentArg );
+
       }
 
-      var newScript = document.createElement( "script" );
-      newScript.type = "text/javascript";
-      newScript.src = currentArg;
-      newScript.onreadystatechange = createOnReadyStateChangeListener( currentArg );
-      newScript.onload = createOnLoadListener( currentArg );
-      head.appendChild( newScript );
+      return returnObject;
 
-      if ( scriptToRequest[currentArg] === undefined ) {
-        scriptToRequest[currentArg] = [];
+    };
+
+    lib.loaded = function( scriptId ) {
+      var scriptURL = scriptIdToURL[scriptId] || scriptId;
+      if ( executed.indexOf( scriptURL ) !== -1 ) {
+        return;
       }
-      scriptToRequest[currentArg].push( currentRequest );
-      requested.push( currentArg );
+      executed.push( scriptURL );
+      var scriptRequests = scriptToRequest[scriptURL];
+      if ( scriptRequests ) {
+        while ( scriptRequests.length /*> 0*/ ) {
+          scriptRequests[0].loaded++;
+          if ( scriptRequests[0].loaded === scriptRequests[0].scriptCount ) {
+            scriptRequests[0].onload.call( window );
+          }
+          scriptRequests.splice( 0, 1 );
+        }
+      }
+    };
+    loadedStrict = function( scriptURL ) {
+      if ( executed.indexOf( scriptURL ) !== -1 ) {
+        return;
+      }
+      executed.push( scriptURL );
+      var scriptRequests = scriptToRequest[scriptURL];
+      if ( scriptRequests ) {
+        while ( scriptRequests.length /*> 0*/ ) {
+          scriptRequests[0].loaded++;
+          if ( scriptRequests[0].loaded === scriptRequests[0].scriptCount ) {
+            scriptRequests[0].onload.call( window );
+          }
+          scriptRequests.splice( 0, 1 );
+        }
+      }
+    };
 
+    window.lib = lib;
+
+    for ( var i = 0, libInitCallbacksLength = libInitCallbacks.length; i < libInitCallbacksLength; i++ ) {
+      libInitCallbacks[i].call(window);
     }
-
-    return returnObject;
+    libInitCallbacked = true;
 
   };
 
-  lib.loaded = function( scriptId ) {
-    var scriptURL = scriptIdToURL[scriptId] || scriptId;
-    if ( executed.indexOf( scriptURL ) !== -1 ) {
-      return;
-    }
-    executed.push( scriptURL );
-    var scriptRequests = scriptToRequest[scriptURL];
-    if ( scriptRequests ) {
-      while ( scriptRequests.length /*> 0*/ ) {
-        scriptRequests[0].loaded++;
-        if ( scriptRequests[0].loaded === scriptRequests[0].scriptCount ) {
-          scriptRequests[0].onload.call( window );
-        }
-        scriptRequests.splice( 0, 1 );
-      }
-    }
-  };
-  loadedStrict = function( scriptURL ) {
-    if ( executed.indexOf( scriptURL ) !== -1 ) {
-      return;
-    }
-    executed.push( scriptURL );
-    var scriptRequests = scriptToRequest[scriptURL];
-    if ( scriptRequests ) {
-      while ( scriptRequests.length /*> 0*/ ) {
-        scriptRequests[0].loaded++;
-        if ( scriptRequests[0].loaded === scriptRequests[0].scriptCount ) {
-          scriptRequests[0].onload.call( window );
-        }
-        scriptRequests.splice( 0, 1 );
-      }
-    }
-  };
+  // if JSON is not defined and we haven't already tried adding one,
+  if ( !window.JSON/*&& !triedJSON*/ ) {
 
-  window.lib = lib;
+    // create a new script element and assign the properties for the JSON "polyfill".
+    var jsonScript = document.createElement( "script" );
+    jsonScript.type = "text/javascript";
+    jsonScript.src = JSON_PATH;
 
-  for ( var i = 0, libInitCallbacksLength = libInitCallbacks.length; i < libInitCallbacksLength; i++ ) {
-    libInitCallbacks[i].call(window);
+    // create a flag that says whether libInit had already been "re-called"
+    var recalled = false;
+
+    // add a ready-state-change handler
+    jsonScript.onreadystatechange = function() { //todo: check this/these
+
+      // if the script is loaded, and libInit have not been "re-called" yet, set the flag and call this function
+      if ( (this.readyState === "loaded" || this.readyState === "complete") && !recalled ) {
+        recalled = true;
+        libInit();
+      }
+
+    };
+
+    // add an onload handler (listening both events just in case)
+    jsonScript.onload = function() {
+
+      // set the flag to true and call libInit if it hadn't been called yet
+      if ( !recalled ) {
+        recalled = true;
+        libInit();
+      }
+    };
+
+    // add this element inside the head
+    head.appendChild( jsonScript );
+
+    return;
+
   }
-  libInitCallbacked = true;
 
-};
-
-// if JSON is not defined and we haven't already tried adding one,
-if ( !window.JSON/*&& !triedJSON*/ ) {
-
-  // create a new script element and assign the properties for the JSON "polyfill".
-  var jsonScript = document.createElement( "script" );
-  jsonScript.type = "text/javascript";
-  jsonScript.src = JSON_PATH;
-
-  // create a flag that says whether libInit had already been "re-called"
-  var recalled = false;
-
-  // add a ready-state-change handler
-  jsonScript.onreadystatechange = function() { //todo: check this/these
-
-    // if the script is loaded, and libInit have not been "re-called" yet, set the flag and call this function
-    if ( (this.readyState === "loaded" || this.readyState === "complete") && !recalled ) {
-      recalled = true;
-      libInit();
-    }
-
-  };
-
-  // add an onload handler (listening both events just in case)
-  jsonScript.onload = function() {
-
-    // set the flag to true and call libInit if it hadn't been called yet
-    if ( !recalled ) {
-      recalled = true;
-      libInit();
-    }
-  };
-
-  // add this element inside the head
-  head.appendChild( jsonScript );
-
-  return;
-
-}
-
-libInit();
+  libInit();
 
 })( window );
